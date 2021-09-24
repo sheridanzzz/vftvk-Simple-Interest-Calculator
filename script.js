0ce8f4a06e5df8e72f1ca800b8a190be27de2129
Sheridan Gomes <42735781+sheridanzzz@users.noreply.github.com>
2021-09-24 12:41:05 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    //variable for principal
+    var principal = document.getElementById("principal").value;
+
+    //checks if principal is less than or equal to 0
+    if(principal <= 0)
+    {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus;
+    }
+    //variable for rate
+    var rate = document.getElementById("rate").value;
+    //variable for years
+    var years = document.getElementById("years").value;
+    //variable for interest
+    var interest = principal * years * rate /100;
+    //variable for year
+    var year = new Date().getFullYear()+parseInt(years);
     
+    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
+}
+
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
 }
         
