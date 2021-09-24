0856b4747f1bc6523be0922669d96ca56deeddb5
sheridan gomes <srei0008@student.monash.edu>
2021-09-24 03:14:09 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function compute()
     //variable for year
     var year = new Date().getFullYear()+parseInt(years);
     
-    var result = document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
+    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>"
 }
 
 function updateRate() 
